274cbafe142f0420f3c5f0d979588dd613593456
Nachiket497 <1000nachiket@gmail.com>
2024-07-18 20:15:48 +0530

    added zen mode

--- nvim/lua/usr/core/keymaps.lua
+++ nvim/lua/usr/core/keymaps.lua
@@ -14,19 +14,36 @@ keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decremen
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set(
+    "n",
+    "<leader>sx",
+    "<cmd>close<CR>",
+    { desc = "Close current split" }
+) -- close current split window
 
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+keymap.set(
+    "n",
+    "<leader>tx",
+    "<cmd>tabclose<CR>",
+    { desc = "Close current tab" }
+) -- close current tab
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+keymap.set(
+    "n",
+    "<leader>tf",
+    "<cmd>tabnew %<CR>",
+    { desc = "Open current buffer in new tab" }
+) --  move current buffer to new tab
 
 keymap.set("n", "<leader>ss", "<cmd>wa<CR>", { desc = "Save all" }) -- Save all and close
 keymap.set("n", "<leader>qq", "<cmd>qa<CR>", { desc = "Close all" }) -- Close all
 
-
 keymap.set("n", "x", '"_x')
 
-keymap.set("n", "Z", '<cmd>lua vim.diagnostic.disable()<CR>')
+keymap.set("n", "Zd", "<cmd>lua vim.diagnostic.disable()<CR>")
+keymap.set("n", "Ze", "<cmd>lua vim.diagnostic.enable()<CR>")
 
+keymap.set("n", "zz", "<cmd>:ZenMode<CR>")
+keymap.set("n", "zt", "<cmd>:Twilight<CR>")
--- nvim/lua/usr/plugins/comment.lua
+++ nvim/lua/usr/plugins/comment.lua
@@ -1,48 +1,51 @@
 return {
-  "numToStr/Comment.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    "JoosepAlviste/nvim-ts-context-commentstring",
-  },
-  config = function()
-    -- import comment plugin safely
-    local comment = require("Comment")
+    "numToStr/Comment.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+        "JoosepAlviste/nvim-ts-context-commentstring",
+    },
+    config = function()
+        -- import comment plugin safely
+        local comment = require("Comment")
 
-    local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+        local ts_context_commentstring =
+            require("ts_context_commentstring.integrations.comment_nvim")
 
-    comment.setup {
-    pre_hook = function (ctx)
-        local U = require "Comment.utils"
+        comment.setup({
+            pre_hook = function(ctx)
+                local U = require("Comment.utils")
 
-        local location = nil
-        if ctx.ctype == U.ctype.blockwise then
-            location = require("ts_context_commentstring.utils").get_cursor_location()
-        elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-            location = require("ts_context_commentstring.utils").get_visual_start_location()
-        end
+                local location = nil
+                if ctx.ctype == U.ctype.blockwise then
+                    location =
+                        require("ts_context_commentstring.utils").get_cursor_location()
+                elseif
+                    ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V
+                then
+                    location =
+                        require("ts_context_commentstring.utils").get_visual_start_location()
+                end
 
-        return require("ts_context_commentstring.internal").calculate_commentstring {
-            key = ctx.ctype == U.ctype.linewise and "__default" or "__multiline",
-            location = location,
-        }
+                return require("ts_context_commentstring.internal").calculate_commentstring({
+                    key = ctx.ctype == U.ctype.linewise and "__default"
+                        or "__multiline",
+                    location = location,
+                })
+            end,
+        })
     end,
-    }
-
-  end,
 }
 
-
-
- -- opts = {
- --      config = {
- --        cpp = "// %s",
- --        verilog = "// %s",
- --        systemverilog = "// %s",
- --      },
- --    },
- --
- --    -- enable comment
-    -- comment.setup({
-    --   -- for commenting tsx, jsx, svelte, html files
-    --   pre_hook = ts_context_commentstring.create_pre_hook(),
-    -- })
+-- opts = {
+--      config = {
+--        cpp = "// %s",
+--        verilog = "// %s",
+--        systemverilog = "// %s",
+--      },
+--    },
+--
+--    -- enable comment
+-- comment.setup({
+--   -- for commenting tsx, jsx, svelte, html files
+--   pre_hook = ts_context_commentstring.create_pre_hook(),
+-- })
